cbc8fce0255f4572e3ff4405c9f40c16b8ec92ee
Gino Paul Mendoza Capio <ginocapio@gmail.com>
2020-02-23 16:51:25 -0700

    Add search and result section along with api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      answer: ''
+    };
+  }
+
+  getResultFromNewtonAPI() {
+    const expression = document.getElementById("expression").value;
+    const myHeaders = new Headers();
+    const myRequest = new Request('https://newton.now.sh/simplify/' + expression, {
+      method: 'GET',
+      headers: myHeaders,
+      cache: 'default',
+    });
+
+    fetch(myRequest)
+    .then(resp => resp.json())
+    .then(function(data) {
+      console.log(data);
+      const result = document.getElementById("result");
+      result.innerText = data.result;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header>
+            <h1 className="text-center mb-5">Go Nimbly - Software Engineer Interview Project</h1>
+          </header>
+
+          <div className="container">
+            {/*search section*/}
+            <div className="row search">
+              <div className="search mx-auto">
+                <input id="expression" className="mx-2" type="text" placeholder="ex. 1+1" />
+                <button className="btn btn-primary mx-2" onClick={this.getResultFromNewtonAPI}>
+                  simplify
+                </button>
+              </div>
+            </div>
+
+            {/* Results section */}
+            <div >
+              Answer:
+              <span id="result">
+                {this.state.answer}
+              </span>
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
