9de98426e7479c4d6debcb4e61293996f424470f
Gino Paul Mendoza Capio <ginocapio@gmail.com>
2020-02-23 21:15:18 -0700

    Fix x margin on latest result

--- src/components/Result/result.js
+++ src/components/Result/result.js
@@ -4,7 +4,7 @@ class Result extends Component {
   render() {
     // Add primary border and bigger margin if latest result
     const wrapperClasses =
-        this.props.newest ? 'row font-weight-bold search my-5 mx-3 border border-primary rounded p-4 w-100' : 'row search m-1 border rounded p-4 w-100'
+        this.props.newest ? 'row font-weight-bold search my-5 mx-1 border border-primary rounded p-4 w-100' : 'row search m-1 border rounded p-4 w-100'
     return (
         <div className={wrapperClasses}  >
           <div className=" col-lg-4 col-sm-12">
